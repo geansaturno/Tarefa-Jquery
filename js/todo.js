20a652cb84028ca59f54cb0bdcb83f7a58501cfd
Gean Saturno <cronosnaeg@gmail.com>
2016-07-05 00:22:01 -0300

    Aplicação do boilerplate

--- js/todo.js
+++ js/todo.js
@@ -2,10 +2,15 @@ $(function(){
 
     $('#tarefa').keydown(function(event){
 
-
-
         if(event.which === 13){
             console.log('Enter precionado');
         }
     });
+
+    $('.tarefa-delete').click(onTarefaDelete);
 })
+
+
+function onTarefaDelete(){
+    console.log("Adeus");
+}
